ca2a076f946f22cec6458c20ce563aecdf2534dc
Aditya(Ady) Kalra <aditya_kalra@ymail.com>
2018-02-27 17:49:59 +0000

    Update Starting your ML model.py

--- Starting your ML model.py
+++ Starting your ML model.py
@@ -5,8 +5,14 @@ main_file_path = '../input/train.csv'
 # read the data and store data in DataFrame titled melbourne_data
 melbourne_data = pd.read_csv(main_file_path)
 
+# print a summary of the data in Melbourne data
+print(melbourne_data.describe())
+
 # print all columns 
 print(melbourne_data.columns)
 
-# print a summary of the data in Melbourne data
-print(melbourne_data.describe())
+# store the series of prices separately as melbourne_yearbuilt_data.
+melbourne_yearbuilt_data = melbourne_data.YearBuilt
+
+# the head command returns the top few lines of data.
+print(melbourne_yearbuilt_data.head())
